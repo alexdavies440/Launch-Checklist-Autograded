388b63e47692d0732f535f762ca2feccf4cafabe
alexdavies440 <alexdavies440@gmail.com>
2024-08-11 18:01:55 -0500

    Empty check finally working properly

--- script.js
+++ script.js
@@ -5,26 +5,23 @@ window.addEventListener("load", function () {
 
     console.log("script.js loaded");
 
-    let pilot = document.querySelector("input[name=pilotName]").value;
-    let copilot = document.querySelector("input[name=copilotName]").value;
-    
-    let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
-    fuelLevel = parseInt(fuelLevel);
-    
-    let cargoLevel = document.querySelector("input[name=cargoMass]").value;
-    cargoLevel = parseInt(cargoLevel);
-    
-    let list = document.getElementById("faultyItems");
-    
-    
-    
     let form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
-    
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName]").value;
+
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        fuelLevel = parseInt(fuelLevel);
+
+        let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+        cargoLevel = parseInt(cargoLevel);
+
+        let list = document.getElementById("faultyItems");
+        
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
         
-        event.preventDefault();
-
+        
     });
     
     
@@ -38,5 +35,5 @@ window.addEventListener("load", function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     })
-    
+
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -21,145 +21,135 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
 function validateInput(testInput) {
-let validated;
+
     if (testInput === "") {
-        validated =  "Empty";
+        return "Empty";
     }
     else if (isNaN(testInput)) {
-        validated =  "Not a Number";
+        return "Not a Number";
     }
     else if (typeof(parseInt(testInput)) === "number") {
-        validated = "Is a Number";
+        return "Is a Number";
     }
-    return validated;
 };
 
 // Functions to be used in formSubmission
+function isEmpty(pilot, copilot, fuelLevel, cargoLevel) {
+    if (
+        validateInput(pilot) === "Empty" ||
+        validateInput(copilot) === "Empty" ||
+        validateInput(fuelLevel) === "Empty" ||
+        validateInput(cargoLevel) === "Empty"
+    
+    ) {
+            window.alert("All fields required!");
+        }
+    return "Empty";
+}
+
+
 function checkPilot(pilot) {
-    if (validateInput(pilot) === "Empty") {
-        window.alert("All fields required!");
-    }
-    else if (validateInput(pilot) === "Is a Number") {
-        window.alert("Cannot be a number");
+    
+    if (validateInput(pilot) === "Is a Number") {
+        window.alert("Pilot cannot be a number");
     }
     else if (validateInput(pilot) === "Not a Number") {
     
         let pilotStatus = document.getElementById("pilotStatus")
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-        pilotStatus.style.visibility = "visible";
+        //pilotStatus.style.visibility = "visible"; //just faultItems?
     }
 }
 
 function checkCopilot(copilot) {
-    if (validateInput(copilot) === "Empty") {
-        window.alert("All fields required!");
-    }
-    else if (validateInput(copilot) === "Is a Number") {
-        window.alert("Cannot be a number");
+    
+    if (validateInput(copilot) === "Is a Number") {
+        window.alert("Copilot cannot be a number");
     }
     else if (validateInput(copilot) === "Not a Number") {
         let copilotStatus = document.getElementById("copilotStatus");
         copilotStatus.innerHTML = `Copilot ${copilot} is ready for launch`;
-        copilotStatus.style.visibility = "visible";
+        //copilotStatus.style.visibility = "visible"; //just faultyItems?
     }
 }
 
 function checkFuelLevel(fuelLevel) {
-    if (validateInput(fuelLevel) === "Empty") {
-        window.alert("All fields required!");
-    }
-    else if (validateInput(fuelLevel) === "Not a Number") {
-        window.alert("Must be a number");
+    
+    if (validateInput(fuelLevel) === "Not a Number") {
+        window.alert("Fuel Level must be a number");
     }
     else if (validateInput(fuelLevel) === "Is a Number" && fuelLevel < 10000) {
-        document.getElementById("faultyItems").style.visibility = "visible";
+        //document.getElementById("faultyItems").style.visibility = "visible"; //Not sure if checking visibility will cause test to fail, will try .includes first
         document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
-        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
-        document.getElementById("launchStatus").style.color = "red";
+        // document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch"
+        // document.getElementById("launchStatus").style.color = "red";
     }
-    // PROBABLY NEED IF STATEMENT FOR OVER 10000 HERE
+    // Ideal condition
     else if (validateInput(fuelLevel) === "Is a Number" && fuelLevel > 10000) {
-        document.getElementById("faultyItems").style.visibility = "visible";
+        //document.getElementById("faultyItems").style.visibility = "visible"; //Item not faulty
         document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch";
-        document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch"
-        document.getElementById("launchStatus").style.color = "green";
+        // document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch"
+        // document.getElementById("launchStatus").style.color = "green";
     }
 
 }
 
 function checkCargoLevel(cargoLevel) {
-    if (validateInput(cargoLevel) === "Empty") {
-        window.alert("All fields required!");
-    }
-    else if (validateInput(cargoLevel) === "Not a Number") {
-        window.alert("Must be a number");
+    
+    if (validateInput(cargoLevel) === "Not a Number") {
+        window.alert("Cargo Mass must be a number");
     }
     else if (validateInput(cargoLevel) === "Is a Number" && cargoLevel > 10000) {
         document.getElementById("faultyItems").style.visibility = "visible";
         document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
-        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
-        document.getElementById("launchStatus").style.color = "red";
+        // document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+        // document.getElementById("launchStatus").style.color = "red";
     }
-    //PROBABLY NEED IF STATEMENT FOR LESS THAN 10000 HERE
+    // Ideal condition
     else if (validateInput(cargoLevel) === "Is a Number" && cargoLevel < 10000) {
-        document.getElementById("faultyItems").style.visibility = "visible";
+        //document.getElementById("faultyItems").style.visibility = "visible"; //see fuelLevel =^
         document.getElementById("cargoStatus").innerHTML = "Cargo mass low enough for launch";
-        document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch"
-        document.getElementById("launchStatus").style.color = "green";
+        // document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch"
+        // document.getElementById("launchStatus").style.color = "green";
     }
 }
-// function finalCheck() {
-//     //May refactor this whole section and declare these variables globally to simplify above functions so i can pass them into this one
-//     let fuelStatus = document.getElementById("fuelStatus").innerHTML
-//     let cargoStatus = document.getElementById("cargoStatus").innerHTML;
-//     let launchStatus = document.getElementById("launchStatus").innerHTML;
+function checkStatus(document, list) {
+    //May refactor this whole section and declare these variables globally to simplify above functions so i can pass them into this one
+    let fuelStatus = document.getElementById("fuelStatus").innerHTML;
+    let cargoStatus = document.getElementById("cargoStatus").innerHTML;
+    let launchStatus = document.getElementById("launchStatus").innerHTML;
 
-//     if (
-//         fuelStatus === "Fuel level high enough for launch" 
-//         && cargoStatus === "Cargo mass low enough for launch"
-//     ) {
-//         launchStatus = "Shuttle is Ready for Launch";
-//         launchStatus.style.color = "green";
-//     } 
-//     else {
-//         launchStatus = "Shuttle not ready for launch";
-//         launchStatus.style.color = "red";
-//     }
-
-// }
+    if (fuelStatus === "Fuel level too low for launch" || cargoStatus === "Cargo mass too heavy for launch") {
+        launchStatus = "Shuttle not ready for launch";
+        launchStatus.style.color = "red";
+        list.style.visibility = "visible";
+    }
+    else if (
+        fuelStatus === "Fuel level high enough for launch" 
+        && cargoStatus === "Cargo mass low enough for launch"
+    ) {
+        launchStatus = "Shuttle is Ready for Launch";
+        launchStatus.style.color = "green";
+        list.style.visibility = "visible";
+    } 
+}
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     //Wondering if I'm supposed to update the list status within this function since it takes a document and a list?
     //document.body.style.backgroundColor = "aqua"; // To see if function is being passed
     console.log("formSubmission called");
 
-    function finalCheck(document, list) {
-        //May refactor this whole section and declare these variables globally to simplify above functions so i can pass them into this one
-        list = document.getElementById("faultyItems")
-        let fuelStatus = document.getElementById("fuelStatus").innerHTML
-        let cargoStatus = document.getElementById("cargoStatus").innerHTML;
-        let launchStatus = document.getElementById("launchStatus").innerHTML;
+    let firstCheck = isEmpty(pilot, copilot, fuelLevel, cargoLevel); 
+//checks first to make sure all fields aren't empty before checking values
+    if (firstCheck !== "Empty") {
+        checkPilot(pilot);
+        checkCopilot(copilot);
+        checkFuelLevel(fuelLevel);
+        checkCargoLevel(cargoLevel);
     
-        if (
-            fuelStatus === "Fuel level high enough for launch" 
-            && cargoStatus === "Cargo mass low enough for launch"
-        ) {
-            launchStatus = "Shuttle is Ready for Launch";
-            launchStatus.style.color = "green";
-        } 
-        else {
-            launchStatus = "Shuttle not ready for launch";
-            launchStatus.style.color = "red";
-        }
-    
+        //checkStatus(document, list);
     }
-
-    checkPilot(pilot);
-    checkCopilot(copilot);
-    checkFuelLevel(fuelLevel);
-    checkCargoLevel(cargoLevel);
-    finalCheck(document, list);
-
+    
 }
 
 
