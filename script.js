1597242746e55d4e5e3b8ad2ce79242358d5834c
alexdavies440 <alexdavies440@gmail.com>
2024-08-11 20:35:49 -0500

    Had to backtrack. Reworking validation

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ window.addEventListener("load", function () {
 
     console.log("script.js loaded");
 
+    
+    
+    
     let form = document.querySelector("form");
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+    form.addEventListener("submit", function(event) {
+
         let pilot = document.querySelector("input[name=pilotName]").value;
         let copilot = document.querySelector("input[name=copilotName]").value;
-
+        
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
-        //fuelLevel = parseInt(fuelLevel); //Something about this causes isEmpty() function to fail for number fields
-
+        //fuelLevel = parseInt(fuelLevel);
+        
         let cargoLevel = document.querySelector("input[name=cargoMass]").value;
         //cargoLevel = parseInt(cargoLevel);
-
+        
         let list = document.getElementById("faultyItems");
         
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
         
-        
+        event.preventDefault();
+
     });
     
     
@@ -35,5 +39,5 @@ window.addEventListener("load", function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     })
-
+    
  });
